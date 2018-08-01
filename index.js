066c82893685bf7f73f905bb9ae27b2f3adc9830
marudor <marudor@marudor.de>
2018-08-01 15:58:21 +0200

    v1.0.1

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = () => ({
+  plugins: [
+    '@babel/plugin-proposal-export-default-from',
+    ['@babel/plugin-proposal-optional-chaining', { loose: false }],
+    ['@babel/plugin-proposal-nullish-coalescing-operator', { loose: false }],
+
+    '@babel/plugin-proposal-export-namespace-from',
+
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-syntax-import-meta',
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
+    '@babel/plugin-proposal-json-strings',
+  ],
+});
